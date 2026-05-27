9b962eb54720e8ddd43a4d2b4b37c827beef37cf
timlin <timlin23953@gmail.com>
2026-05-27 22:09:58 +0800

    加入專案檔案。

---
+++ WordCards/frmEditWord.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WordCards
+{
+    partial class frmEditWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grpPhonogram = new System.Windows.Forms.GroupBox();
+            this.txtPhonogram = new System.Windows.Forms.TextBox();
+            this.grpSoundPath = new System.Windows.Forms.GroupBox();
+            this.txtSoundPath = new System.Windows.Forms.TextBox();
+            this.grpExplain = new System.Windows.Forms.GroupBox();
+            this.txtExplain = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.grpPhonogram.SuspendLayout();
+            this.grpSoundPath.SuspendLayout();
+            this.grpExplain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtWord);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(20, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 85);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "單字";
+            // 
+            // grpPhonogram
+            // 
+            this.grpPhonogram.Controls.Add(this.txtPhonogram);
+            this.grpPhonogram.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpPhonogram.Location = new System.Drawing.Point(20, 117);
+            this.grpPhonogram.Name = "grpPhonogram";
+            this.grpPhonogram.Size = new System.Drawing.Size(379, 85);
+            this.grpPhonogram.TabIndex = 1;
+            this.grpPhonogram.TabStop = false;
+            this.grpPhonogram.Text = "音標";
+            // 
+            // txtPhonogram
+            // 
+            this.txtPhonogram.Location = new System.Drawing.Point(6, 38);
+            this.txtPhonogram.Name = "txtPhonogram";
+            this.txtPhonogram.Size = new System.Drawing.Size(367, 39);
+            this.txtPhonogram.TabIndex = 0;
+            // 
+            // grpSoundPath
+            // 
+            this.grpSoundPath.Controls.Add(this.txtSoundPath);
+            this.grpSoundPath.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpSoundPath.Location = new System.Drawing.Point(20, 208);
+            this.grpSoundPath.Name = "grpSoundPath";
+            this.grpSoundPath.Size = new System.Drawing.Size(379, 85);
+            this.grpSoundPath.TabIndex = 2;
+            this.grpSoundPath.TabStop = false;
+            this.grpSoundPath.Text = "音檔路徑";
+            // 
+            // txtSoundPath
+            // 
+            this.txtSoundPath.Location = new System.Drawing.Point(6, 40);
+            this.txtSoundPath.Name = "txtSoundPath";
+            this.txtSoundPath.Size = new System.Drawing.Size(367, 39);
+            this.txtSoundPath.TabIndex = 0;
+            // 
+            // grpExplain
+            // 
+            this.grpExplain.Controls.Add(this.txtExplain);
+            this.grpExplain.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpExplain.Location = new System.Drawing.Point(20, 299);
+            this.grpExplain.Name = "grpExplain";
+            this.grpExplain.Size = new System.Drawing.Size(379, 284);
+            this.grpExplain.TabIndex = 3;
+            this.grpExplain.TabStop = false;
+            this.grpExplain.Text = "解釋";
+            // 
+            // txtExplain
+            // 
+            this.txtExplain.Location = new System.Drawing.Point(6, 40);
+            this.txtExplain.Multiline = true;
+            this.txtExplain.Name = "txtExplain";
+            this.txtExplain.Size = new System.Drawing.Size(367, 226);
+            this.txtExplain.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnSave.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSave.Location = new System.Drawing.Point(305, 589);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 50);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Enabled = false;
+            this.txtWord.Location = new System.Drawing.Point(6, 38);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(367, 39);
+            this.txtWord.TabIndex = 1;
+            // 
+            // frmEditWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 650);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.grpExplain);
+            this.Controls.Add(this.grpSoundPath);
+            this.Controls.Add(this.grpPhonogram);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MinimizeBox = false;
+            this.Name = "frmEditWord";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "編輯單字";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpPhonogram.ResumeLayout(false);
+            this.grpPhonogram.PerformLayout();
+            this.grpSoundPath.ResumeLayout(false);
+            this.grpSoundPath.PerformLayout();
+            this.grpExplain.ResumeLayout(false);
+            this.grpExplain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grpPhonogram;
+        private System.Windows.Forms.TextBox txtPhonogram;
+        private System.Windows.Forms.GroupBox grpSoundPath;
+        private System.Windows.Forms.TextBox txtSoundPath;
+        private System.Windows.Forms.GroupBox grpExplain;
+        private System.Windows.Forms.TextBox txtExplain;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtWord;
+    }
+}
